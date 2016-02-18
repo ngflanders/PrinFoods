b1497eb846b091b41258f79e98daa6af6c0e48fe
Nicholas <ngflanders@gmail.com>
2016-02-18 16:14:56 -0600

    Parsing for dining times, isn't working yet

--- app/src/main/java/com/ngflanders/android/prinfoods/DiningHoursActivity.java
+++ app/src/main/java/com/ngflanders/android/prinfoods/DiningHoursActivity.java
@@ -3,15 +3,23 @@ package com.ngflanders.android.prinfoods;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class DiningHoursActivity extends AppCompatActivity {
 
@@ -21,9 +29,7 @@ public class DiningHoursActivity extends AppCompatActivity {
     private TextView dateTextView;
     private Switch dateSwitch;
 
-    String now;
-    String tom;
-
+    String now_dmd, tom_dmd, tod_d, tom_d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,27 +44,31 @@ public class DiningHoursActivity extends AppCompatActivity {
         c.add(Calendar.DAY_OF_YEAR, 1);
         final Date tomorrow = c.getTime();
 
-        DateFormat df = new SimpleDateFormat("EEEE, MMMM dd");
-        now = df.format(today);
-        tom = df.format(tomorrow);
+        DateFormat dmd = new SimpleDateFormat("EEEE, MMMM dd");
+        now_dmd = dmd.format(today);
+        tom_dmd = dmd.format(tomorrow);
 
-        dateTextView.setText(now);
+        DateFormat d = new SimpleDateFormat("EEEE");
+        tod_d = d.format(today);
+        tom_d = d.format(tomorrow);
+
+        dateTextView.setText(now_dmd);
 
         dateSwitch = (Switch) findViewById(R.id.dateSwitch);
         dateSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                dateTextView.setText(isChecked ? tom : now);
+                dateTextView.setText(isChecked ? tom_dmd : now_dmd);
             }
         });
 
+        updateTimes();
 
     }
 
 
     public void onButtonClick(View v) {
         Intent intent = new Intent(this, DiningMenuActivity.class);
-
 
         switch (v.getId()) {
             case R.id.BreakfastButton:
@@ -74,19 +84,36 @@ public class DiningHoursActivity extends AppCompatActivity {
                 throw new RuntimeException("Unknown button ID");
         }
 
-        intent.putExtra(EXTRA_DATE, !dateSwitch.isChecked() ? now : tom);
-
+        intent.putExtra(EXTRA_DATE, !dateSwitch.isChecked() ? now_dmd : tom_dmd);
         startActivity(intent);
 
     }
 
+    private void updateTimes() {
 
-    public void launchDiningMenuActivity(View view) {
-        Intent intent = new Intent(this, DiningMenuActivity.class);
-        startActivity(intent);
+        Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("MealTimes");
+        //query.whereEqualTo("dayOfWeek", !dateSwitch.isChecked() ? tod_d : tom_d);
+        query.whereEqualTo("dayOfWeek", "Thursday");
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+                if (e == null) {
+
+                    for (ParseObject item : objects) {
+
+                    }
+
+                } else {
+                    Log.d(getClass().getSimpleName(), "Error: " + e.getMessage());
+
+                }
+            }
+        });
+
     }
 
-    private void updateData() {
-
-    }
 }
--- app/src/main/java/com/ngflanders/android/prinfoods/HomeActivity.java
+++ app/src/main/java/com/ngflanders/android/prinfoods/HomeActivity.java
@@ -4,15 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
 
 public class HomeActivity extends AppCompatActivity {
-
-    private Button diningButton;
-    private Button pubButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +17,6 @@ public class HomeActivity extends AppCompatActivity {
 
         Parse.enableLocalDatastore(this);
         Parse.initialize(this);
-
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("nick", "isawesome");
-        testObject.saveInBackground();
 
     }
 
